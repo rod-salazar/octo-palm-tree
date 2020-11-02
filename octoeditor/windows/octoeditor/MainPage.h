421573afb01ab73771058d6c63d1784dd9694008
Rodrigo Salazar <rodrigos@fb.com>
2020-11-01 20:28:03 -0800

    Reinstalled react native windows, launches now

---
+++ octoeditor/windows/octoeditor/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::octoeditor::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::octoeditor::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
